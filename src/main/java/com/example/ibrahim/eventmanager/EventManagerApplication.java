ce8d60b7b86e8bf92938bf1bf3e06aef8e1867ce
ibrahim4851 <erogluih2001@gmail.com>
2022-07-08 17:55:40 +0300

    initial

---
+++ src/main/java/com/example/ibrahim/eventmanager/EventManagerApplication.java
@@ -0,0 +1,32 @@
+package com.example.ibrahim.eventmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagerApplication.class, args);
+    }
+
+}
+
+/* ------ PROJECT DESCRIPTION -------- */
+/*
+Verilerinizi yönetmek için bir model olarak bir Event sınıfı oluşturunuz.
+
+
+Aşağıda Event classının sahip olması gereken fiedler mevcuttur.
+
+etkinlik adı
+başlangıç tarihi
+bitiş tarihi
+kota
+katılımcı sayısı
+
+
+Bu model için, etkinlik ekleme, güncelleme, silme operasyonlarını içeren bir spring boot restful api örneği beklenmektedir. Controllerin API isteğine JSON Objesi olarak dönmesi gerekmektedir. İsteğe karşılık HTTP status kodlarının ayrıca eklenmesi beklenmektedir.(
+Örn. Başarılı bir işlem için OK kodu olan 200 dönülebilir.)
+
+ */
