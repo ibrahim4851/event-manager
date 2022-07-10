e9d22a369cff453ab29460eb56636322f491af74
ibrahim4851 <erogluih2001@gmail.com>
2022-07-10 14:03:37 +0300

    response entity

--- src/main/java/com/example/ibrahim/eventmanager/controller/EventController.java
+++ src/main/java/com/example/ibrahim/eventmanager/controller/EventController.java
@@ -3,6 +3,8 @@ package com.example.ibrahim.eventmanager.controller;
 import com.example.ibrahim.eventmanager.entity.Event;
 import com.example.ibrahim.eventmanager.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,24 +18,25 @@ public class EventController {
     EventService eventService;
 
     @GetMapping("/getall")
-    public List<Event> getAll(){
-        return eventService.findAll();
+    public ResponseEntity<List<Event>> getAll(){
+        return new ResponseEntity<>(eventService.findAll(), HttpStatus.OK);
     }
 
     @PutMapping("/edit")
-    public Event editEvent(@RequestBody Event event){
+    public ResponseEntity<Event> editEvent(@RequestBody Event event){
         Event editedEvent = eventService.customizeEvent(event);
-        return editedEvent;
+        return new ResponseEntity<>(editedEvent, HttpStatus.OK);
     }
 
     @PostMapping("/create")
-    public Event createEvent(@RequestBody Event event){
-        return eventService.addEvent(event);
+    public ResponseEntity<Event> createEvent(@RequestBody Event event){
+        return new ResponseEntity<>(eventService.addEvent(event), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteEvent(@PathVariable(value = "id") Long id){
+    public ResponseEntity deleteEvent(@PathVariable(value = "id") Long id){
         eventService.deleteEvent(id);
+        return new ResponseEntity("Event Successfully deleted", HttpStatus.OK);
     }
 
 }
